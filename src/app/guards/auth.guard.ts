9754a0d66231c4b7cedb6d745c7cae18fd212f70
ROLAND PARADO <roland@stampede-design.com>
2024-08-25 16:10:30 +0800

    fix guard setup

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +9,36 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private loginService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.loginService.isAuthenticated()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']); // Redirect to login page if not authenticated
-      return false;
-    }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+      return this.checkLogin();
+
+  }
+
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      
+      return this.canActivate(next, state);
+
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+      // console.log('canload')
+      return this.checkLogin();
+
+  }
+
+  async checkLogin(): Promise<boolean> {
+	if (this.loginService.isAuthenticated()) {
+		return true;
+	  } else {
+		this.router.navigate(['/login']); // Redirect to login page if not authenticated
+		return false;
+	  }
   }
 }
