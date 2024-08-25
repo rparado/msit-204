f80a7c83f0b466e68509dc193272f6f317972973
ROLAND PARADO <roland@stampede-design.com>
2024-08-25 16:41:59 +0800

    add actionsheet to user profile

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,20 +1,55 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent  implements OnInit {
-  @Input() pageTitle: string = '';
+	@Input() pageTitle: string = '';
 
-  constructor(private authService: AuthService) { }
+	constructor(
+		private authService: AuthService,
+		private actionSheetCtrl: ActionSheetController
+	) { }
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  logout() {
-	window.location.href='/login'
-  }
+	logout() {
+		window.location.href='/login'
+	}
+	profile() {
 
+	}
+	async presentActionSheet() {
+		const actionSheet = await this.actionSheetCtrl.create({
+		  header: 'Settings',
+		  buttons: [
+			{
+			  text: 'Profile',
+			  handler: () => {
+				this.profile();
+			  },
+			},
+			{
+			  text: 'Logout',
+			  handler: () => {
+				this.logout();
+			  },
+			},
+			{
+			  text: 'Cancel',
+			  role: 'cancel',
+			  data: {
+				action: 'cancel',
+			  },
+			},
+		  ],
+		});
+	
+		await actionSheet.present();
+	}
+	
 }
