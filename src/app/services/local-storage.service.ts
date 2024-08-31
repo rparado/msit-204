f36bf3a73fb511747f90708d53401401ae9f9694
ROLAND PARADO <roland@stampede-design.com>
2024-08-31 23:34:17 +0800

    fix service

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -6,12 +6,22 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class LocalStorageService {
 
-  private countSubject = new BehaviorSubject<number>(0);
+  private countSubject = new BehaviorSubject<number>(this.getStoredCount());
   count$ = this.countSubject.asObservable();
 
   constructor() { }
 
+  private getStoredCount(): number {
+    const storedValue = localStorage.getItem('billCount');
+    const count = storedValue ? parseInt(storedValue, 10) : 0;
+    return isNaN(count) ? 0 : count;
+  }
+
   updateCount(value: number): void {
-    this.countSubject.next(value);
+    // Only update if the new value is different from the current one
+    if (value !== this.countSubject.getValue()) {
+      this.countSubject.next(value);
+      localStorage.setItem('billCount', value.toString());
+    }
   }
 }
